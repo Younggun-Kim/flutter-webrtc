0de1e9b754d7f90a8e88695f4b1e78555947c6e4
younggun.kim <younggun.kiim@ikoob.com>
2025-08-01 08:57:56 +0900

    feat: MethodChannel import 추가

--- android/src/main/java/com/cloudwebrtc/webrtc/MethodCallHandlerImpl.java
+++ android/src/main/java/com/cloudwebrtc/webrtc/MethodCallHandlerImpl.java
@@ -92,6 +92,7 @@ import java.util.UUID;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.view.TextureRegistry;
